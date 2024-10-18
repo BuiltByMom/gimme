74336ba963cb28cd3960aaf76106741f3f5ec042
Daniil Polienko <donyaschefer@gmail.com>
2024-10-18 17:21:28 +0300

    chore: add key

--- packages/lib/common/Switch.tsx
+++ packages/lib/common/Switch.tsx
@@ -15,6 +15,7 @@ export function Switch(props: {
 						'bg-primary w-full basis-1/2 rounded-[14px] py-2.5 text-sm font-bold transition-colors',
 						props.value !== option.value ? 'bg-transparent text-grey-700 font-normal' : ''
 					)}
+					key={option.value}
 					onClick={() => props.onSelectValue(option.value)}>
 					{option.title}
 				</button>
